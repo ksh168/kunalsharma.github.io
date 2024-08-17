803075f03a5312c60827ea40c36a714f88b17cf0
ksh168 <mailkunalsharma99@gmail.com>
2024-08-18 00:26:44 +0530

    added hamburger menu for navbar for phones

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { navigationLinks } from '../config/navigation';
-import '../styles/navbar.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { navigationLinks } from "../config/navigation";
+import "../styles/navbar.css";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav>
-      <ul>
+      <div className="hamburger" onClick={toggleMenu}>
+        {/* Each div represents one bar of the hamburger icon */}
+        <div className={`bar ${isMenuOpen ? "open" : ""}`}></div>
+        <div className={`bar ${isMenuOpen ? "open" : ""}`}></div>
+        <div className={`bar ${isMenuOpen ? "open" : ""}`}></div>
+      </div>
+      <ul className={`nav-links ${isMenuOpen ? "open" : ""}`}>
         {navigationLinks.map((link) => (
           <li key={link.path}>
-            <Link to={link.path}>{link.label}</Link>
+            <Link to={link.path} onClick={() => setIsMenuOpen(false)}>
+              {link.label}
+            </Link>
           </li>
         ))}
       </ul>
